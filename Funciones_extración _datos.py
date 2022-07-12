1584d31b024d3dcc7371453a7880666c2379d641
pablomartinescobar1 <pablo.martin.escobar@alumnos.upm.es>
2022-07-12 16:03:10 +0200

    Update Funciones_extración _datos.py

--- Funciones_extración _datos.py
+++ Funciones_extración _datos.py
@@ -1,46 +1,105 @@
 from influxdb import InfluxDBClient
+import csv
+import time
+import requests
+from zipfile import ZipFile
 
-client = InfluxDBClient(host='localhost', port=8086)
-client.create_database('pyexample')
-client.get_list_database()
-client.switch_database('pyexample')
+def api_extraction(url):
 
-json_body = [
-    {
-        "measurement": "brushEvents",
-        "tags": {
-            "user": "Carol",
-            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-        },
-        "time": "2018-03-28T8:01:00Z",
-        "fields": {
-            "duration": 127
-        }
-    },
-    {
-        "measurement": "brushEvents",
-        "tags": {
-            "user": "Carol",
-            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-        },
-        "time": "2018-03-29T8:04:00Z",
-        "fields": {
-            "duration": 132
-        }
-    },
-    {
-        "measurement": "brushEvents",
-        "tags": {
-            "user": "Carol",
-            "brushId": "6c89f539-71c6-490d-a28d-6c5d84c0ee2f"
-        },
-        "time": "2018-03-30T8:02:00Z",
-        "fields": {
-            "duration": 129
-        }
-    }
-]
-client.write_points(json_body)
+   resp = requests.get(url,verify=False) #Realizamos la petición a la API donde estan los nombres de los fotometro
+   posts = resp.json()
+   photometers_good_keys={}
+   for i in posts:
+      photometers_good_keys[i['name']] = {'latitude':i['info_location']['latitude'],'longitude':i['info_location']['longitude']}
+   return(photometers_good_keys)   #Devuelve una listas una con los nombres, la longitud y latitud asociado
+   
+def api_extraction2(url):
 
-results = client.query("SELECT \"duration\" FROM brushEvents WHERE time >= '2018-03-28T00:00:00Z' AND time <= '2018-08-30T00:00:00Z' AND 'user' = Carol")
-print(list(results))
+    resp = requests.get(url,verify=False) #Realizamos la petición a la API donde estan los nombres de los fotometro
+    posts = resp.json()
+    photometers_good_keys={}
+    for i in posts:
+        photometers_good_keys[i['name']] = i
+    return(photometers_good_keys)#Devuelve un diccionario una con los nombres como key , los values son los campos de la Api 
+
+def db_acces(hostdb ,portdb,usernamedb,passworddb,database,start,end,name,good_keys): #Solo te devuelve los datos de un fotometro(user)
+
+  client = InfluxDBClient(host=hostdb, port=portdb, username=usernamedb, password=passworddb ,ssl=True, verify_ssl=True) #Accede a la base de datos mediante un cliente 
+  client.switch_database(database)
+  data_photometer=[]
+  data_photometer = client.query("SELECT * FROM brushEvents WHERE time >= '"+ start +"' AND time <= '"+ end +"' AND \"user\" = '"+ name +"'") # Nos devuelve los datosl fotometros
+  #con el nombre de user y metiendo la fecha de inicio y de fin
+  data_photometer=list(data_photometer)[0]
+  for i in range(len(data_photometer)):
+    data_photometer[i]['latitude']=good_keys[data_photometer[i]['user']]['latitude'] #Añadimos el apartado de latitud correspondiente al fotometro
+    data_photometer[i]['longitude']=good_keys[data_photometer[i]['user']]['longitude'] #Añadimos el apartado de longitud correspondiente al fotometro
+  return(data_photometer)  #Devuelve los el apartado fields de los objetos en influxdb
+
+def csv_generator(data,name,user): #Los datos deben ser de un solo fotometro (user) para añadir una cabecera con datos especificos 
+  dict = api_extraction2('https://api.stars4all.eu/photometers') #Realizamos la petición a la API donde esta toda la info de los fotometros
+  headers =[
+      "# Community Standard Skyglow Data Format 1.0",
+'# URL: https://www.darksky.org/wp-content/uploads/bsk-pdf-manager/47_SKYGLOW_DEFINITIONS.PDF',
+'# Number of header lines: 35',
+'# This data is released under the following license: ODbL 1.0 http://opendatacommons.org/licenses/odbl/summary/',
+'# Device type: SQM-LE',
+'# Instrument ID: Dahlem_tower_le',
+#Aquí abajo añadimos a la cabecera la información correspondiente a su usuario en la Api
+'# Data supplier: '+ str(dict[user]['tester'])+',https://api.stars4all.eu/photometers'
+'# Location name: '+str( dict[user]['info_location']['country'])+'-'+str( dict[user]['info_location']['region'])+'-'+str( dict[user]['info_location']['town'])+'-'+str( dict[user]['info_location']['place']),
+'# Position (lat, lon, elev(m)):'+str( dict[user]['info_location']['latitude'])+','+str( dict[user]['info_location']['longitude']) ,
+'# Local timezone: ',
+'# Time Synchronization: GPS',
+'# Moving / Stationary position: STATIONARY',
+'# Moving / Fixed look direction: FIXED'
+'# Number of channels: 1',
+'# Filters per channel: HOYA CM-500',
+'# Measurement direction per channel: 0., 0.',
+'# Field of view (degrees): 20',
+'# Number of fields per line: 6',
+'# SQM serial number: 1687',
+'# SQM firmware version: 4-3-21',
+'# SQM cover offset value: -0.11',
+'# SQM readout test ix: i,00000004,00000003,00000021,00001687',
+'# SQM readout test rx: r, 18.73m,0000000004Hz,0000130978c,0000000.284s, 031.2C',
+'# SQM readout test cx: c,00000019.69m, 0000300.000s, 023.2C,00000008.71m, 029.3C',
+'# Comment: ',
+'# Comment: ',
+'# Comment: ',
+'# Comment: ',
+'# Comment: ',
+'# blank line 30',
+'# blank line 31',
+'# blank line 32',
+'# UTC Date & Time, Local Date & Time, Temperature, Counts, Frequency, MSAS',
+'# YYYY-MM-DDTHH:mm:ss.fff;YYYY-MM-DDTHH:mm:ss.fff;Celsius;number;Hz;mag/arcsec^2',
+'# END OF HEADER']
+
+  f= open('STARS4ALL'+str(name)+str('.csv'), "w") #Creamos el archivo y añadimos las cabeceras
+  for i in headers:
+    f.write(i +'\n')
+  f.close()
+  with open('STARS4ALL'+str(name)+str('.csv'), mode='a',newline= '') as File: #Añadimos los parametros 
+    writer = csv.writer(File)
+    writer.writerow(['name , tamb , tsky , mag , tstamp , latitude , longitude'])    
+    keys=['user','brushId','time','duration','duration2','latitude','longitude']
+    for i in data:
+       writer.writerow([i[k]for k in keys]) 
+
+def csv_generator2(data,name): #Los datos deben ser de todos los usuarios(data)
+
+  with open('STARS4ALL'+str(name)+str('.csv'), mode='w',newline= '') as File: #Añadimos los parametros 
+    writer = csv.writer(File)
+    writer.writerow(['name , tamb , tsky , mag , tstamp , latitude , longitude'])    
+    keys=['user','brushId','time','duration','duration2','latitude','longitude']
+    for i in data:
+       writer.writerow([i[k]for k in keys])
+
+
+
+def compress(archivos,zip): #comprime los archivos 
+
+    myzip = ZipFile(str(zip)+'.zip', 'w')
+    for i in archivos:
+       myzip.write(i)
+    myzip.close()
