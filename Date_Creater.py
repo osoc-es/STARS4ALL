5016ece90d3eb76028406211cbde5d709b7b2eb4
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-07-13 12:36:26 +0200

    Update Date_Creater.py

--- Date_Creater.py
+++ Date_Creater.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+#This funtion is designed so we can access to the time of the last month before 
+    #the activation of the main, so we can use it to obtein the name of the file and path.
+
 def current_date_format():
     date = datetime.now()
     months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
