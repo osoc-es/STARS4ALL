80c12f55f1f633d262a45db11b6c9e28355f92ec
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-07-13 12:24:38 +0200

    actualización

--- Stars4All_App.py
+++ Stars4All_App.py
@@ -1,10 +1,15 @@
 import CSV_ZENODO
 from CSV_ZENODO import Upload_Zenodo
+from Date_Creater import current_date_format
 
+
+
+Time = current_date_format()
 
 Access_Token = 'p25CSlnib5XnUTzEzgXNphLL4RzUwp23SF4YBhu8jMrq41xO5hboH6roEv1d'
-Path = "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Star4All\\stars4all-july-2020.csv"
-Filename = 'stars4all-july-2020'
+Filename = 'stars4all-%s' % Time
+Path = "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Star4All\\%s.csv" % Filename
+print(Filename)
 Title = 'Stars4ll_Tester'
 Description = 'Tester for Stars4All'
 name = 'Daniel Moreno'
@@ -20,4 +25,4 @@ meta_data = {
 
 def UPload_files():
     Upload_Zenodo(Access_Token, meta_data, Path, Filename)
-UPload_files()
+#UPload_files()
