c2bcf2609f475febd2b2faa992f1b355dac19c35
Daniel Moreno <danielmorenoperez071103@gmail.com>
2022-07-13 17:20:05 +0200

    actualización sin querer

--- Stars4All_App.py
+++ Stars4All_App.py
@@ -1,27 +1,49 @@
 import CSV_ZENODO
 from CSV_ZENODO import Upload_Zenodo
 from Date_Creater import current_date_format
-
+import zipfile
+import shutil
 
 
 Time = current_date_format()
 
 Access_Token = 'p25CSlnib5XnUTzEzgXNphLL4RzUwp23SF4YBhu8jMrq41xO5hboH6roEv1d'
-Filename = 'stars4all-%s' % Time
-Path = "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Star4All\\%s.csv" % Filename
-Title = 'Stars4ll_Tester'
+Filename_Row = 'stars4all-Row-%s' % Time
+Filename_Final = 'stars4all-Final-%s' % Time
+Title_Row = 'Stars4ll_Tester_Row'
+Title_Final = 'Stars4ll_Tester_Final'
+
 Description = 'Tester for Stars4All'
 name = 'Daniel Moreno'
 Affiliation = 'OSOC'
 Type = 'poster'
-meta_data = {
+testeo = "test"
+
+meta_data_Row = {
      'metadata': {
-            'title': Title,
+            'title': Title_Row,
          'upload_type': Type,
         'description': Description,
         'creators': [{'name': name,
                     'affiliation': Affiliation}] } }
 
+meta_data_Final = {
+     'metadata': {
+            'title': Title_Final,
+         'upload_type': Type,
+        'description': Description,
+        'creators': [{'name': name,
+                    'affiliation': Affiliation}] } }
+
+path_row = "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV_Zip\\Stars4All-RowData-%s" % testeo
+path_final = "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV_Zip\\Stars4All-FinalData-%s" % testeo
+
+def File_Zip(Filename):
+    shutil.make_archive("C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV_Zip\\Stars4All-FinalData-%s" % Filename, "zip", "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV\\Final")
+    shutil.make_archive("C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV_Zip\\Stars4All-RowData-%s" % Filename, "zip", "C:\\Users\\Daniel Moreno\\Desktop\\Osoc-2022\\Testeos\\CSV\\Row")
 def UPload_files():
-    Upload_Zenodo(Access_Token, meta_data, Path, Filename)
+    Upload_Zenodo(Access_Token, meta_data_Row, path_row, Filename_Row)
+    Upload_Zenodo(Access_Token, meta_data_Final, path_final, Filename_Final)
+File_Zip(testeo)
+
 UPload_files()
