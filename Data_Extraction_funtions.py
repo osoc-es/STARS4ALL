34dd5e872952de277d42731f873db895713c9c4d
pablomartinescobar1 <pablo.martin.escobar@alumnos.upm.es>
2022-07-19 11:15:03 +0200

    Update Data_Extraction_funtions.py

--- Data_Extraction_funtions.py
+++ Data_Extraction_funtions.py
@@ -136,10 +136,10 @@ def work_flow(inicio,fin,path1,path2):#Path1 es donde se genera el documento con
                 for count in i:
                     writer.writerow([count[k]for k in keys])
     def csv_generator2(data,name,dict,path): #Los datos deben ser de todos los usuarios(data)    
-
-        for i in range(len(data)):
-            data[i]['latitude']=dict[data[i]['user']]['latitude'] #Añadimos el apartado de latitud correspondiente al fotometro
-            data[i]['longitude']=dict[data[i]['user']]['longitude'] #Añadimos el apartado de longitud correspondiente al fotometro
+        print(data)
+        #for i in range(len(data)):
+        #    data[i]['latitude']=dict[data[i]['user']]["info_location"]['latitude'] #Añadimos el apartado de latitud correspondiente al fotometro
+        #    data[i]['longitude']=dict[data[i]['user']]["info_location"]['longitude'] #Añadimos el apartado de longitud correspondiente al fotometro
 
         with open(path +  'STARS4ALL-'+str(name)+'-'+str('.csv'), mode='w',newline= '') as File: #Añadimos los parametros 
             writer = csv.writer(File)   
@@ -171,7 +171,7 @@ def work_flow(inicio,fin,path1,path2):#Path1 es donde se genera el documento con
 
     for j in usuarios:
         name1= str(name)+str(j)
-        csv_generator(db_access(hostdb ,portdb,usernamedb,passworddb,database,inicio,fin,j),name1,j,dict,path1)
+        #csv_generator(db_access(hostdb ,portdb,usernamedb,passworddb,database,inicio,fin,j),name1,j,dict,path1)
         print( 'Generating csv file for' + j )
 
     f2= open(path2 + 'STARS4ALL-'+str(name)+'-'+ str('.csv'), "w") #Creamos el archivo y añadimos las cabeceras
